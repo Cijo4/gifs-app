9e60f8077a1e64e91c32d62d66fa1d4b20520625
Jose Sanchez <josedeveloper01@gmail.com>
2021-10-15 19:08:17 +0100

    Trabajando historial de busqueda en sidebar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../services/gifs.service';
+
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
@@ -10,10 +12,13 @@ export class BusquedaComponent {
  
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+
+  constructor(private gifService: GifsService){}
+
   buscar(){
     const valor= this.txtBuscar.nativeElement.value;
 
-    console.log(valor)
+    this.gifService.buscarGifs( valor );
 
     this.txtBuscar.nativeElement.value = '';
   }
